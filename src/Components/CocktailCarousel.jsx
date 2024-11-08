0431bdc5e55d18a894df89420dbfbad9525588be
Alex <409073@niuitmo.ru>
2024-11-08 10:39:49 +0300

    removed translation, added new Image component

--- src/Components/CocktailCarousel.jsx
+++ src/Components/CocktailCarousel.jsx
@@ -1,8 +1,8 @@
 import React, {useState} from 'react';
-import {CocktailCard} from './CocktailCard';
 import {cocktails} from './CocktailList';
 import {ReactComponent as ArrowForward} from "../icons/arrow_forward_ios_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
 import {ReactComponent as ArrowBack} from "../icons/arrow_back_ios_24dp_E8EAED_FILL0_wght400_GRAD0_opsz24.svg";
+import {Image} from "./Image";
 
 export const CocktailCarousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -26,7 +26,7 @@ export const CocktailCarousel = () => {
             </button>
             <div className="carousel__track">
                 <div className="carousel__slide">
-                    <CocktailCard cocktail={cocktails[currentIndex]}/>
+                    <Image cocktail={cocktails[currentIndex]}/>
                 </div>
             </div>
 
